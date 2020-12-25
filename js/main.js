e398c645a9f04377a112d5f45a69931983e2ddc6
dolphius21 <johnrudolphlee@gmail.com>
2020-12-25 17:47:59 +0800

    change icon to close icon when closing the nav

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const nav = document.querySelector('.nav');
+const toggle = document.querySelector('#toggle-icon');
 
 document.getElementById('toggle').onclick = () => {
-   (nav.classList.contains('show'))? nav.classList.remove('show')
-   : nav.classList.add('show');
+   if (nav.classList.contains('show')) {
+      nav.classList.remove('show');
+      toggle.classList.remove('fa-times');
+      toggle.classList.add('fa-bars');
+   } else {
+      nav.classList.add('show');
+      toggle.classList.remove('fa-bars');
+      toggle.classList.add('fa-times');
+      
+   }
 };
